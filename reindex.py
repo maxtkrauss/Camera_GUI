7835dd15755e348b1fea198133336b5c53e3811a
Matthew Morales <mtthwmrls9@gmail.com>
2025-08-28 13:53:08 -0600

    Implement renaming in reindex function

--- reindex.py
+++ reindex.py
@@ -18,7 +18,7 @@ def generate_reindex_file (path, ignorelist=[], outfile='map.json'):
             index += 1
     
     with open(outfile, 'w') as f:
-        f.write(json.dumps(mappings))
+        f.write(json.dumps(mappings, indent=4))
         print(f"WROTE INDEX FILE TO {outfile}")
 
 def apply_reindexing (path, ignorelist=[], index_file='map.json', template="#"):
@@ -38,14 +38,27 @@ def apply_reindexing (path, ignorelist=[], index_file='map.json', template="#"):
             if index is not None:
                 _, ext = os.path.splitext(f.name)
                 new_name = template.replace('#', str(index))+ext
+                f.rename(f.parent / new_name)
                 print(f"RENAMING {f.name} TO {new_name}")
 
 
-ignorelist = ['cubert', 'thorlabs']
-map_file = 'map.json'
-source_dir = './test/cubert'
-target_dir = './test/thorlabs'
+# generate_reindex_file('./test/cubert', ['cubert', 'thorlabs'])
+# apply_reindexing('./test/cubert', ['cubert', 'thorlabs'], 'map.json', template="image_#_cubert")
+# apply_reindexing('./test/thorlabs', ['cubert', 'thorlabs'], 'map.json', template="image_#_thorlabs")
 
-generate_reindex_file (source_dir, ignorelist, map_file)
-apply_reindexing(source_dir, ignorelist, map_file, template="image_#_thorlabs")
-apply_reindexing(target_dir, ignorelist, map_file, template="image_#_cubert")
+ignorelist = ['cubert', 'thorlabs']
+map_file_train = 'map_train.json'
+map_file_valid = 'map_valid.json'
+basedir = '/scratch/general/nfs1/u1344001/data/EndoDBV1/'
+
+source_dir = Path(basedir) / 'training/cubert'
+target_dir = Path(basedir) / 'training/thorlabs'
+generate_reindex_file (source_dir, ignorelist, map_file_train)
+apply_reindexing(source_dir, ignorelist, map_file_train, template="image_#_thorlabs")
+apply_reindexing(target_dir, ignorelist, map_file_train, template="image_#_cubert")
+
+source_dir = Path(basedir) / 'validation/cubert'
+target_dir = Path(basedir) / 'validation/thorlabs'
+generate_reindex_file (source_dir, ignorelist, map_file_valid)
+apply_reindexing(source_dir, ignorelist, map_file_valid, template="image_#_thorlabs")
+apply_reindexing(target_dir, ignorelist, map_file_valid, template="image_#_cubert")
